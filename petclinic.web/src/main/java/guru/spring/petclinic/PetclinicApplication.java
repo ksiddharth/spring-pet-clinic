bc2479986d1faebc8bca19bff86396b7870ea4e0
Siddharth Kumar <siddharth.manikandan@gmail.com>
2019-11-14 00:56:17 +0530

    added auto versioning for sub modules and a sample sysout

--- petclinic.web/src/main/java/guru/spring/petclinic/PetclinicApplication.java
+++ petclinic.web/src/main/java/guru/spring/petclinic/PetclinicApplication.java
@@ -8,6 +8,7 @@ public class PetclinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PetclinicApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
