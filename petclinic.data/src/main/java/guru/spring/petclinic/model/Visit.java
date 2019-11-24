c688c55911cdce162c5fdc12cf4a1152e414599e
Siddharth Kumar <siddharth.manikandan@gmail.com>
2019-11-24 18:18:39 +0530

    added visit entity and updated owner entity to have a reference of pets. Closes #26.

---
+++ petclinic.data/src/main/java/guru/spring/petclinic/model/Visit.java
@@ -0,0 +1,33 @@
+package guru.spring.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+    private Pet pet;
+    private String description;
+    private LocalDate date;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
