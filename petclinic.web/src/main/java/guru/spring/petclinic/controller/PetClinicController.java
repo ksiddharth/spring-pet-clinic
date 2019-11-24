ff76f67be02fb2a8334a7b365ddb6e9f78b36aa5
Siddharth Kumar <siddharth.manikandan@gmail.com>
2019-11-24 17:04:08 +0530

    Fixed location of static resources and configuration properties

--- petclinic.web/src/main/java/guru/spring/petclinic/controller/PetClinicController.java
+++ petclinic.web/src/main/java/guru/spring/petclinic/controller/PetClinicController.java
@@ -3,12 +3,14 @@ package guru.spring.petclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PetClinicController {
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String indexHandler(Model model) {
-        return "index";
+    public ModelAndView indexHandler(Model model) {
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
     }
 }
