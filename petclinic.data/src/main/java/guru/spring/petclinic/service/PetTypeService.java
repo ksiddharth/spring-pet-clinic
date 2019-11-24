f19e53e85d1262dfa9a46639dbbbab2f42abbadd
Siddharth Kumar <siddharth.manikandan@gmail.com>
2019-11-24 18:33:16 +0530

    added pet type service. Closes #29.

---
+++ petclinic.data/src/main/java/guru/spring/petclinic/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.spring.petclinic.service;
+
+import guru.spring.petclinic.model.PetType;
+import guru.spring.petclinic.service.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ petclinic.data/src/main/java/guru/spring/petclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package guru.spring.petclinic.service.map;
+
+import guru.spring.petclinic.model.PetType;
+import guru.spring.petclinic.service.PetTypeService;
+import net.bytebuddy.implementation.bind.annotation.Super;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService <PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void deleteObject(PetType petType) {
+        super.deleteByObject(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
